0d8f5eee588d44ff89be1413fdae8cc80506c137
Thomas Bouquet <thomas.bouquet@rte-france.com>
2023-07-12 17:36:50 +0200

    add todo

--- gridcapa-core-cc-app/src/test/java/com/farao_community/farao/gridcapa_core_cc/app/preprocessing/RaoParametersServiceTest.java
+++ gridcapa-core-cc-app/src/test/java/com/farao_community/farao/gridcapa_core_cc/app/preprocessing/RaoParametersServiceTest.java
@@ -49,7 +49,7 @@ class RaoParametersServiceTest {
                 && expectedLoopFlowConstraintCountries.containsAll(actualLoopFlowConstraintCountries)
                 && actualLoopFlowConstraintCountries.containsAll(expectedLoopFlowConstraintCountries));
 
-        // null pointer exception
+        // TODO: understand null pointer exception
         // SearchTreeParameters searchTreeParameters = raoParameters.getRaUsageLimitsPerContingencyParameters().getExtension(SearchTreeParameters.class);
         // assertEquals(10.0, searchTreeParameters.getNetworkActionParameters().getAbsoluteNetworkActionMinimumImpactThreshold());
 
