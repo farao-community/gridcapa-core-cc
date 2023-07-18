b54c11a09a81d63c11c0e72c81447403e59fb362
Thomas Bouquet <thomas.bouquet@rte-france.com>
2023-07-18 12:01:24 +0200

    HourlyRaoRequestTest

---
+++ gridcapa-core-cc-api/src/test/java/com/farao_community/farao/gridcapa_core_cc/api/resource/HourlyRaoRequestTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+package com.farao_community.farao.gridcapa_core_cc.api.resource;
+
+import com.farao_community.farao.minio_adapter.starter.MinioAdapter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+/**
+ * @author Thomas Bouquet {@literal <thomas.bouquet at rte-france.com>}
+ */
+public class HourlyRaoRequestTest {
+
+    private MinioAdapter minioAdapter;
+    private HourlyRaoRequest hourlyRaoRequest;
+
+    @BeforeEach
+    void setUp() {
+        minioAdapter = Mockito.mock(MinioAdapter.class);
+        Mockito.when(minioAdapter.generatePreSignedUrl(Mockito.any())).thenReturn("http://url");
+        hourlyRaoRequest = new HourlyRaoRequest(minioAdapter, "instant", "file/path/network", "file/path/cb", "file/path/refprog", "file/path/glsk", "file/path/raoParameters", "path/to/destination");
+    }
+
+    @Test
+    void checkHourlyRaoRequest() {
+        assertEquals("instant", hourlyRaoRequest.getRaoRequestInstant());
+        assertEquals("file/path/network", hourlyRaoRequest.getNetworkFileUrl());
+        assertEquals("file/path/cb", hourlyRaoRequest.getCracFileUrl());
+        assertEquals("file/path/raoParameters", hourlyRaoRequest.getRaoParametersFileUrl());
+        assertEquals("path/to/destination", hourlyRaoRequest.getResultsDestination());
+        assertEquals("id", hourlyRaoRequest.toRaoRequest("id").getId());
+        assertEquals("http://url", hourlyRaoRequest.toRaoRequest("id").getCracFileUrl());
+        assertEquals("http://url", hourlyRaoRequest.toRaoRequest("id").getNetworkFileUrl());
+        assertEquals(1957570048, hourlyRaoRequest.hashCode());
+    }
+
+    @Test
+    void testEquals() {
+        HourlyRaoRequest sameInstantHourlyRaoRequest = new HourlyRaoRequest(minioAdapter, "instant", "file/path/network", "file/path/cb", "file/path/refprog", "file/path/glsk", "file/path/raoParameters", "path/to/destination");
+        HourlyRaoRequest differentInstantHourlyRaoRequest = new HourlyRaoRequest(minioAdapter, "otherInstant", "file/path/network", "file/path/cb", "file/path/refprog", "file/path/glsk", "file/path/raoParameters", "path/to/destination");
+        assertEquals(hourlyRaoRequest, hourlyRaoRequest);
+        assertEquals(hourlyRaoRequest, sameInstantHourlyRaoRequest);
+        assertNotEquals(hourlyRaoRequest, differentInstantHourlyRaoRequest);
+        assertNotEquals(hourlyRaoRequest, 0);
+    }
+
+}
