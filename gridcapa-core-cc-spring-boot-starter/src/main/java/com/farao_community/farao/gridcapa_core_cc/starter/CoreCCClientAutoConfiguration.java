d0f25ffb2af9f7679a2104091698aadff96a7767
thiriondan <daniel.thirion@rte-france.com>
2025-01-17 10:33:33 +0100

    Fix context with correctly scanning CoreCCClient bean
    Signed-off-by: thiriondan <daniel.thirion@rte-france.com>

---
+++ gridcapa-core-cc-spring-boot-starter/src/main/java/com/farao_community/farao/gridcapa_core_cc/starter/CoreCCClientAutoConfiguration.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.gridcapa_core_cc.starter;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Ameni Walha {@literal <ameni.walha at rte-france.com>}
+ */
+@Configuration
+@EnableConfigurationProperties(CoreCCClientProperties.class)
+@ComponentScan
+public class CoreCCClientAutoConfiguration {
+}
